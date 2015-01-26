5943b987a37a79f46c564e9f073c1588e31702ac
keoghpe@tcd.ie <keoghpe@tcd.ie>
2015-01-26 13:46:58 +0000

    Add full title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Finyl Vinal"
+    assert_select "title", "Finyl Vinal"
   end
 
   test "should get help" do
