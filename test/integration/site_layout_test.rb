6dc0a3ee1dbbd666dae1fe0f32cad6e71173531e
keoghpe@tcd.ie <keoghpe@tcd.ie>
2015-01-31 11:45:33 +0000

    Refactor edit and new for user

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,11 +1,16 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:pete)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", login_path
   end
 end
